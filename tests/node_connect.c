edd14fd552c30e2b7553c99cd55b5006b7139611
Anatole Lefort <anatole.lefort@imdea.org>
2018-04-05 10:40:26 +0200

    now test the failure and reconnection mecanism for nodes
        - add a new pthread barrier to sync the recovering nodes if needed
        - pass the barrier as pthread_create arg such that nodes can use
              the appropriate barrier
        - add global variables for thread_ids, nodes, and nodes' status
          so that the main thread can read data from the others
        - add wait mecanism so that the main thread knows when it can
          ,i.e. the cluster is stable, start killing or reconnect nodes

--- tests/node_connect.c
+++ tests/node_connect.c
@@ -15,51 +15,138 @@
 // are finished and are returning appropriate error codes.
 // In other words, this is should not be taken as a usage example!
 
-pthread_barrier_t *pth_barrier;
+#define NUMBER_OF_FAILURES 2
+
+pthread_t pth[NUMBER_OF_NODES];
+struct node *nodes[NUMBER_OF_NODES];
+int status[NUMBER_OF_NODES];
 
 struct arg {
     id_t id;
     struct cluster_config *conf;
+    pthread_barrier_t *barrier;
 };
 
+//NOTE: Also works fine without the barriers.
+//      Pthreads were chosen over processes because of the simple
+//      synchronization mecanism, but since none is actually needed,
+//      let's use proccesses and remove that UGLY node_stop() public primitive
+//      and instead send a signal to the process to stop the main event loop
+//TODO ReWrite using processes (fork) instead of threads (pthread)
 void* node_create_run(void* arg) {
     id_t id = ((struct arg *) arg)->id;
     struct cluster_config *conf = ((struct arg *) arg)->conf;
+    pthread_barrier_t *pth_barrier = ((struct arg *) arg)->barrier;
 
     pthread_barrier_wait(pth_barrier);
-    struct node *n = node_init(conf, id);
+    nodes[id] = node_init(conf, id);
+    status[id] = 1;
 
     pthread_barrier_wait(pth_barrier);
-    node_start(n);
+    node_start(nodes[id]);
 
-    pthread_barrier_wait(pth_barrier);
-    node_free(n);
+    if (nodes[id]->comm->accepted_count != NUMBER_OF_NODES)
+        printf("[%u] Failed to connect to the whole cluster"
+			" (%u connected peers)\n", id, nodes[id]->comm->accepted_count);
+
+    node_free(nodes[id]);
+
+    status[id] = 0;
 
     pthread_exit(NULL);
 }
 
+//Scenario:
+//  Start all the nodes
+//  Once they are all connected, kill some random ones
+//  After connections were lost, restart the faulty nodes
+//  Wait until all the connections successfully re-establish
+//  Succesfully exits
 int main(int argc, char *argv[]) {
+    //Init the RNG
+    time_t t;
+    srand((unsigned) time(&t));
+
     //TODO Create some better public config helper function to make it easier
     fill_cluster_config(&conf, NUMBER_OF_NODES, id, group_membership, addresses, ports);
 
     //The plan is to start each node in a separate thread, with an event_base loop in each
-    pthread_t pth[NUMBER_OF_NODES];
+    pthread_barrier_t *start_pthb;
+    pthread_barrier_t *restart_pthb;
     evthread_use_pthreads();
 
-    pth_barrier = malloc(sizeof(pthread_barrier_t));
-    if (pthread_barrier_init(pth_barrier, NULL, NUMBER_OF_NODES) != 0) {
-        printf("Can not init pthread barrier");
+    start_pthb = malloc(sizeof(pthread_barrier_t));
+    if (pthread_barrier_init(start_pthb, NULL, NUMBER_OF_NODES) != 0) {
+        printf("Can not init start pthread barrier");
+        return EXIT_FAILURE;
+    }
+    restart_pthb = malloc(sizeof(pthread_barrier_t));
+    if (pthread_barrier_init(restart_pthb, NULL, NUMBER_OF_FAILURES) != 0) {
+        printf("Can not init restart pthread barrier");
         return EXIT_FAILURE;
     }
 
     //Let's now create the nodes
     struct arg args[NUMBER_OF_NODES];
     for(int i=0; i<NUMBER_OF_NODES; i++) {
-	args[i].id = conf.id[i]; args[i].conf = &conf;
+	args[i].id = conf.id[i]; args[i].conf = &conf; args[i].barrier = start_pthb;
         pthread_create(pth+i, NULL, node_create_run, args+i);
     }
 
-    //If needed, let the main thread do some stuffs
+    //Let's wait until connections are successful
+    int connected;
+    do {
+        connected = 0;
+        for(int i=0; i<NUMBER_OF_NODES; i++) {
+	    if (nodes[i])
+                if (nodes[i]->comm->accepted_count == NUMBER_OF_NODES)
+                    connected++;
+        }
+    } while(connected != NUMBER_OF_NODES);
+
+    //Let's terminate some random nodes
+    for(int i=0; i<NUMBER_OF_FAILURES; ) {
+	id_t id = rand() % NUMBER_OF_NODES;
+	if(status[id] != 0) {
+            printf("Closing node %u\n", id);
+            node_stop(nodes[id]);
+            pthread_join(pth[id], NULL);
+	    i++;
+        }
+    }
+
+    //Wait until the nodes are actually dead
+    do {
+        connected = 0;
+        for(int i=0; i<NUMBER_OF_NODES; i++) {
+            if(status[i] != 0)
+                if(nodes[i]->comm->accepted_count == NUMBER_OF_NODES - NUMBER_OF_FAILURES)
+                    connected++;
+        }
+    } while(connected != NUMBER_OF_NODES - NUMBER_OF_FAILURES);
+
+    //Let's then reconnect them
+    for(int i=0; i<NUMBER_OF_NODES; i++) {
+	if(status[i] == 0) {
+	    args[i].id = conf.id[i]; args[i].conf = &conf; args[i].barrier = restart_pthb;
+            pthread_create(pth+i, NULL, node_create_run, args+i);
+        }
+    }
+
+    //And finally wait until connections are back up
+    do {
+        connected = 0;
+        for(int i=0; i<NUMBER_OF_NODES; i++) {
+	    if (status[i] != 0)
+                if (nodes[i]->comm->accepted_count == NUMBER_OF_NODES)
+                    connected++;
+        }
+    } while(connected != NUMBER_OF_NODES);
+
+    //Break the event loop for all nodes
+    for(int i=0; i<NUMBER_OF_NODES; i++) {
+        node_stop(nodes[i]);
+    }
 
     //Wait for the threads to terminate
     for(int i=0; i<NUMBER_OF_NODES; i++) {
@@ -67,5 +154,9 @@ int main(int argc, char *argv[]) {
     }
 
     puts("The test is finished, if nothing was reported, it means it works!\n");
+
+    //Cleanup and exit
+    free(start_pthb);
+    free(restart_pthb);
     return EXIT_SUCCESS;
 }
