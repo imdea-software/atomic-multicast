296d3ebdd99f971fb7db4155f08b0a5290a9ac46
Anatole Lefort <anatole.lefort@imdea.org>
2018-05-10 10:46:23 +0200

    add htable support by wrapping GLib's hash_table implementation

---
+++ src/htable.h
@@ -0,0 +1,30 @@
+#ifndef _HTABLE_H_
+#define _HTABLE_H_
+
+#include <gmodule.h>
+
+typedef void ht_key_t;
+typedef void ht_val_t;
+typedef int (*ht_keyequ_fun)(ht_key_t *, ht_key_t *);
+
+typedef GHashTable htable_t;
+
+#define htable_init(keyequ_fun) \
+    g_hash_table_new(NULL, (GEqualFunc) (keyequ_fun))
+
+#define htable_free(ht) \
+    g_hash_table_destroy((ht))
+
+#define htable_insert(ht, key, val) \
+    g_hash_table_insert((ht), (key), (val))
+
+#define htable_remove(ht, key) \
+    g_hash_table_remove((ht), (key))
+
+#define htable_lookup(ht, key, ret_key, ret_val) \
+    g_hash_table_lookup_extended((ht), (key), (ret_key), (ret_val))
+
+#define htable_foreach(ht, fun, arg) \
+    g_hash_table_foreach((ht), (fun), (arg))
+
+#endif
