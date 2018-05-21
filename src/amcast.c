imdea-software/atomic-multicast

commit 67b90565d82863172670bf0c58fa531e260053e3

safety edit: reject ACCEPT_ACK with higher ballots if « not ahead of time »
    If a LEADER processing ACCEPT_ACKS encounters one with higher ballots than its local ones,
    and those local ones were initialized with a proper ACCEPT command (so not ahead of time),
    reject that ACCEPT_ACK as a safety measure (it might not be necessary, but rejecting it is
    assured not to harm anything).

src/amcast.c
@@ -159,6 +159,10 @@ static void handle_accept_ack(struct node *node, xid_t sid, accept_ack_t *cmd) {
         for(xid_t i=0; i<node->groups->groups_count; i++)
             switch(paircmp(&msg->lballot[i], &cmd->ballot[i])) {
                 case -1:
+                    //Reject commands with higher ballot numbers if already locally initialized
+                    if(paircmp(&msg->lts[i], &default_pair) != 0)
+                        return;
+                    //Only mark for reset if update from non-null value
                     if(paircmp(&msg->lballot[i], &default_pair) != 0)
                         updated_components++;
                     msg->lballot[i] = cmd->ballot[i];

commit 140cae572125a1247746461e4e75d8b711c55199

fix - bug introduced by 67b90565 - this fix keeps lts and lballot evenly updated
    lts[mygroup] is always initialized, so the guard put by 67b90565 was not working for i=mygroup

src/amcast.c
@@ -68,6 +68,7 @@ static void handle_multicast(struct node *node, xid_t sid, message_t *cmd) {
             node->amcast->clock++;
             msg->lts[node->comm->groups[node->id]].time = node->amcast->clock;
             msg->lts[node->comm->groups[node->id]].id = node->comm->groups[node->id];
+            msg->lballot[node->comm->groups[node->id]] = node->amcast->ballot;
             pqueue_push(node->amcast->pending_lts, &msg, &msg->lts[node->comm->groups[node->id]]);
         }
         struct enveloppe rep = {