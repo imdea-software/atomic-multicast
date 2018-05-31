62274a77d6f5c74f9dc901b55c6f434c9eb51be2
Anatole Lefort <anatole.lefort@imdea.org>
2018-05-31 15:26:04 +0200

    emulab - clusterconf_gen: now can generate clusterconf with multiple entries for same host

--- utils/emulab_clusterconf_gen.sh
+++ utils/emulab_clusterconf_gen.sh
@@ -13,15 +13,19 @@ NUMBER_OF_NODES_PER_GROUP=3
 
 cat $TOPOMAP | grep $TOPOMAP_NODE_ID_PREFIX | sort -V | cut -d ":" -f2 > $TMPFILE
 
+NUMBER_OF_NODES=$([ $# -eq 1 ] && echo "$1" || cat $TMPFILE | wc -l)
 N_ID=0
 G_ID=0
 N_PORT=9000
 echo -e "#GENERATED_EMULAB_CLUSTER_CONF" > $OUTFILE
 echo -e "#N_ID\tG_ID\tIP_ADDR\tIP_LIST_PORT" >> $OUTFILE
+while [ $N_ID -lt $NUMBER_OF_NODES ] ; do
 while read N_IP ; do
     echo -e "${N_ID}\t${G_ID}\t${N_IP}\t${N_PORT}" >> $OUTFILE
     N_ID=$(( $N_ID + 1 ))
     [ $(( $N_ID % ${NUMBER_OF_NODES_PER_GROUP} )) -eq 0 ] && G_ID=$(( $G_ID + 1 ))
 done < $TMPFILE
+N_PORT=$(( $N_PORT + 1))
+done
 
 rm $TMPFILE
