e72c8fd72ba924d2ad2d4e8d815e70ef4125fae8
Anatole Lefort <anatole.lefort@imdea.org>
2018-07-11 10:52:44 +0200

    pqueue.h: add function to apply callback function on each elements in pqueue

--- src/pqueue.h
+++ src/pqueue.h
@@ -4,6 +4,7 @@
 typedef void pq_pri_t;
 typedef void pq_val_t;
 typedef int (*pq_pricmp_fun)(pq_pri_t *, pq_pri_t *);
+typedef int (*pq_traverse_fun)(pq_pri_t *, pq_val_t *, void *);
 
 struct pqueue;
 typedef struct pqueue pqueue_t;
@@ -15,5 +16,6 @@ int pqueue_remove(pqueue_t *pq, pq_pri_t *pri);
 pq_val_t *pqueue_peek(pqueue_t *pq);
 pq_val_t *pqueue_pop(pqueue_t *pq);
 unsigned int pqueue_size(pqueue_t *pq);
+void pqueue_foreach(pqueue_t *pq, pq_traverse_fun cb, void *cb_arg);
 
 #endif
