imdea-software/atomic-multicast

commit 8019f67ee9e8ba9a79bd08e0d08819cdc2e242d5

amcast_recovery: implement message delivery pattern in handle_newleader_sync_ack()
    basically copy-pasted from handle_accept_ack()

src/amcast.c
@@ -477,7 +477,44 @@ static void handle_newleader_sync_ack(struct node *node, xid_t sid, newleader_sy
     if(node->amcast->newleader_sync_ack_groupcount < node->groups->node_counts[node->comm->groups[node->id]]/2 + 1)
         return;
     node->amcast->status = LEADER;
-    //TODO add delivery pattern
+    //TODO Check whether computing gts_inf_delivered is useful for recovered messages
+    //TODO CHANGETHIS ugly copy-paste of the delivery pattern
+    int try_next = 1;
+    while(try_next && pqueue_size(node->amcast->committed_gts) > 0) {
+        struct amcast_msg *i_msg = NULL, *j_msg = NULL;
+        try_next = 0;
+        if((i_msg = pqueue_peek(node->amcast->committed_gts)) == NULL) {
+            printf("Failed to peek - %u\n", pqueue_size(node->amcast->committed_gts));
+            return;
+        }
+        if(i_msg->phase == COMMITTED
+                && i_msg->delivered == FALSE) {
+            j_msg = pqueue_peek(node->amcast->pending_lts);
+            if(j_msg != NULL && paircmp(&j_msg->lts[node->comm->groups[node->id]],
+                    &i_msg->gts) < 0
+                    && j_msg->phase != COMMITTED) {
+                return;
+            }
+            if((i_msg = pqueue_pop(node->amcast->committed_gts)) == NULL) {
+                printf("Failed to pop - %u\n", pqueue_size(node->amcast->committed_gts));
+                return;
+            }
+            try_next = 1;
+            struct enveloppe rep = {
+	            .sid = node->id,
+	            .cmd_type = DELIVER,
+	            .cmd.deliver = {
+	                .mid = i_msg->msg.mid,
+                    .ballot = node->amcast->ballot,
+                    .lts = i_msg->lts[node->comm->groups[node->id]],
+                    .gts_inf_delivered = node->amcast->gts_inf_delivered,
+                    .gts = i_msg->gts
+	            },
+	        };
+            send_to_group(node, &rep, node->comm->groups[node->id]);
+            msg->delivered = TRUE;
+        }
+    }
     //TODO add retry pattern for accepted messages
     int retry_message(g_uid_t *lts, struct amcast_msg *msg, struct node *node) {
         return 0;

commit e7bd7af33ee83515050758bc6776862c08187db9

bigfix - amcast handle_accept_ack: set DELIVERED flag to the delivered msg instead of received one
    fix bug (typo?) introduced by 8019f67e and 730fca79 - DELIVERED flag was being set to
    the current message instead of the one being delivered (head of committed_gts is not always the
    current message).

src/amcast.c
@@ -282,7 +282,7 @@ static void handle_accept_ack(struct node *node, xid_t sid, accept_ack_t *cmd) {
 	            },
 	        };
                 send_to_group(node, &rep, node->comm->groups[node->id]);
-                msg->delivered = TRUE;
+                i_msg->delivered = TRUE;
             }
         }
     }
@@ -546,7 +546,7 @@ static void handle_newleader_sync_ack(struct node *node, xid_t sid, newleader_sy
 	            },
 	        };
             send_to_group(node, &rep, node->comm->groups[node->id]);
-            msg->delivered = TRUE;
+            i_msg->delivered = TRUE;
         }
     }
     //TODO add retry pattern for accepted messages