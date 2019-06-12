34ce82653b590e631f7331b67bdff0532f6054fb
Anatole Lefort <anatole.lefort@imdea.org>
2019-06-12 16:16:55 +0200

    bench_runtmux: hooks for libevamcast benches

--- bench/runbench_tmux.sh
+++ bench/runbench_tmux.sh
@@ -37,10 +37,49 @@ run_nodes() {
     done
 }
 
+run_px_nodes() {
+    GROUPS_COUNT=$1
+
+    GROUP_IDS=`seq 0 $((${GROUPS_COUNT} - 1))`
+    TMUX_WINDOW_NAME_PREFIX=px_
+
+    for gid in ${GROUP_IDS} ; do
+        for nid in `seq 0 2` ; do
+            HOST_ID=$(( ( $gid * 3 ) + ${nid} ))
+
+            tmux new-window -n ${TMUX_WINDOW_NAME_PREFIX}${gid}_$nid
+            tmux send-keys " ssh node-$(( $HOST_ID + 1))" Enter
+            tmux send-keys " export LD_LIBRARY_PATH=/usr/local/lib" Enter
+            tmux send-keys " /users/lefort_a/libmcast/build/sample/proposer-acceptor $nid ${AMCAST_DIR}/bench/mcast_conf/paxos-6g3p-group${gid}.conf > /tmp/px${gid}${nid}.log" Enter
+        done
+    done
+}
+
+run_mcast_nodes() {
+    GROUPS_COUNT=$1
+
+    GROUP_IDS=`seq 0 $((${GROUPS_COUNT} - 1))`
+    TMUX_WINDOW_NAME_PREFIX=n_
+
+    for gid in ${GROUP_IDS} ; do
+        for nid in `seq 0 2` ; do
+            HOST_ID=$(( ( $gid * 3 ) + ${nid} ))
+
+            tmux new-window -n ${TMUX_WINDOW_NAME_PREFIX}${gid}_$nid
+            tmux send-keys " ssh node-$(( $HOST_ID + 1))" Enter
+            tmux send-keys " export LD_LIBRARY_PATH=/usr/local/lib" Enter
+            tmux send-keys " /users/lefort_a/libmcast/build/sample/node-simple -n $nid -g $gid -c ${AMCAST_DIR}/bench/mcast_conf/mcast-6g3p.conf -s amcast -p ${AMCAST_DIR}/bench/mcast_conf/paxos-6g3p-group${gid}.conf" Enter
+        done
+    done
+}
 
 tmux -2 new-session -d -s $AMCAST_TMUX_SESSION
 
 run_nodes $AMCAST_BENCH_NUMBER_OF_NODES 0
+
+#run_px_nodes $AMCAST_BENCH_NUMBER_OF_GROUPS
+#sleep 10
+#run_mcast_nodes $AMCAST_BENCH_NUMBER_OF_GROUPS
 
 sleep 10
 
@@ -48,14 +87,18 @@ sleep 10
 
 tmux new-window -n client_0
 tmux send-keys " ssh node-19" Enter
+tmux send-keys " export LD_LIBRARY_PATH=/usr/local/lib" Enter
 for i in `seq 1 $(( $AMCAST_BENCH_NUMBER_OF_CLIENTS / 2))` ; do
     tmux send-keys " (${AMCAST_BIN} ${i} ${AMCAST_BENCH_NUMBER_OF_NODES} ${AMCAST_BENCH_NUMBER_OF_GROUPS} ${AMCAST_BENCH_NUMBER_OF_CLIENTS} 1 < $AMCAST_BENCH_CLUSTER_CONF)> /dev/null &" Enter
+    #tmux send-keys " (${AMCAST_BIN} ${i} ${AMCAST_BENCH_NUMBER_OF_NODES} ${AMCAST_BENCH_NUMBER_OF_GROUPS} ${AMCAST_BENCH_NUMBER_OF_CLIENTS} 1 < ${AMCAST_DIR}/bench/mcast_conf/alt_cluster.conf)> /dev/null &" Enter
 done
 
 tmux new-window -n client_1
 tmux send-keys " ssh node-20" Enter
+tmux send-keys " export LD_LIBRARY_PATH=/usr/local/lib" Enter
 for i in `seq $(( ( $AMCAST_BENCH_NUMBER_OF_CLIENTS / 2 ) + 1 )) $(( $AMCAST_BENCH_NUMBER_OF_CLIENTS ))` ; do
     tmux send-keys " (${AMCAST_BIN} ${i} ${AMCAST_BENCH_NUMBER_OF_NODES} ${AMCAST_BENCH_NUMBER_OF_GROUPS} ${AMCAST_BENCH_NUMBER_OF_CLIENTS} 1 < $AMCAST_BENCH_CLUSTER_CONF)> /dev/null &" Enter
+    #tmux send-keys " (${AMCAST_BIN} ${i} ${AMCAST_BENCH_NUMBER_OF_NODES} ${AMCAST_BENCH_NUMBER_OF_GROUPS} ${AMCAST_BENCH_NUMBER_OF_CLIENTS} 1 < ${AMCAST_DIR}/bench/mcast_conf/alt_cluster.conf)> /dev/null &" Enter
 done
 
 #tmux -2 attach-session -t $AMCAST_TMUX_SESSION
